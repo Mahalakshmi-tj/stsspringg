d51776c922f0d850ee126b51a2dd571fec5eb250
Mahalakshmi.tj <mahalakshmitj.xworkz@gmail.com>
2024-04-06 09:32:13 +0530

    commit

---
+++ Projectfirst/src/main/java/com/springboot/ProjectfirstApplication.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfirstApplication.class, args);
+		
+		System.out.println("welcome to boot....");
+	}
+
+}
